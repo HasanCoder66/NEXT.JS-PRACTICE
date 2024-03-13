95b669881b81bb6ee33f2c7c784b072b6eac4477
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-13 08:27:47 +0500

    2nd video done

--- newwork/pages/components/button/Button.js
+++ newwork/pages/components/button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({text, click}) => {
   return (
-   <button>
+   <button onClick={click}>
     {text}
    </button>
   )
--- newwork/pages/index.js
+++ newwork/pages/index.js
@@ -1,22 +1,29 @@
 
 import Button from "./components/button/Button";
 import Lists from "./components/list/Lists";
-
+import { useState } from "react";
 
 export default function Home() {
-  const dataOfArray = ['be honest' , 'tell truth' ,'never cheat']
+  const [dataOfArray , setDataOfArray] = useState(['be honest' , 'tell truth' ,'never cheat'])
+  // let dataOfArray = 
+  const addHandler = () => {
+    console.log('click working')
+    setDataOfArray([...dataOfArray, 'new item added']);
+    // setDataOfArray.push([...dataOfArray , 'new items add'])
+    console.log('dataOfArray' , dataOfArray)
+  }
   return (
     <>
       <h1>
         Welcome to 2nd video practice with Ishaq Bhojani
       </h1>
      {dataOfArray.map((items)=> (
-      <Lists data={items}/>
+      <Lists data={items} key={items}/>
      ))}
       <h3>
         Oper map ke zariye data display keraya jaraha hai 
       </h3>
-      <Button text='click this button'/>
+      <Button text='click this button to new list' click={addHandler} />
     </>
   );
 }
