da53b65017df281adc43a89ba4eab0b014af807d
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-13 06:42:49 +0500

    video one done

---
+++ newwork/pages/route/hasan.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const hasan = () => {
+  return (
+    <div>
+        <h1>
+        Muhammad Hasan 
+        </h1>
+        <p>Aap ka route/hasan Route chal raha hai</p>
+        <p>Ye aap ka first Next.js Learning Project hai with <b>Sir Ishaq Bhojani</b> </p>
+    </div>
+  )
+}
+
+export default hasan
