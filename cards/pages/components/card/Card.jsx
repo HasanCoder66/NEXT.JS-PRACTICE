3f4e2495c9996f7bed5c78b0a380f14a37d83dcf
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-14 16:30:51 +0500

    video no 3 ki working horahi hai

---
+++ cards/pages/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'antd';
+const { Meta } = Card;
+const CardCmp = ({data}) => (
+  <Card
+  hoverable
+  style={{
+    width: 240,
+  }}
+  // {{console.log(data)}}
+    cover={<img alt="Place Image" src="https://images.unsplash.com/photo-1551749005-6b94ff060954?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwcGxhY2VzfGVufDB8fDB8fHww" />}
+  >
+    <Meta title={data} description="www.instagram.com" />
+  </Card>
+);
+export default CardCmp;
