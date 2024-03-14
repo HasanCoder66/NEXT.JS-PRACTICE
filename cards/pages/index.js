8deab762c0a3199bd9e4b10fe676e44951c3e5cb
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-14 17:15:52 +0500

    break

--- cards/pages/index.js
+++ cards/pages/index.js
@@ -1,18 +1,20 @@
 // import Image from "next/image";
 // import { Inter } from "next/font/google";
-import CardCmp from "./components/card/Card";
 // const inter = Inter({ subsets: ["latin"] });
 // import data from '@/pages/utils/data.json'
+// const {authors , ...others} = data
+{/* <div className='flex justify-evenly mt-[2rem]'>
+ {authors.map((data)=> (
+   ))}
+  </div> */}
+  {/* data={data} */}
+import {CardCmp} from "@/pages/components/CardComponent/CardCmp";
+
+
 export default function Home() {
-  // const {authors , ...others} = data
   console.log(authors);
   return (
     <>
-    {/* <div className='flex justify-evenly mt-[2rem]'>
-     {authors.map((data)=> (
-       ))}
-      </div> */}
-      {/* data={data} */}
       <CardCmp  />
    
     </>
