daa13f7a3ac9518f4990c98942ecc7a082754baa
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-15 14:53:43 +0500

    cards ka project me maha error hai

--- cards/pages/components/CardComponent/CardCmp.jsx
+++ cards/pages/components/CardComponent/CardCmp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'antd';
 const { Meta } = Card;
+
 const CardCmp = () => (
   <Card
     hoverable
--- cards/pages/index.js
+++ cards/pages/index.js
@@ -1,22 +1,17 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
-// const inter = Inter({ subsets: ["latin"] });
-// import data from '@/pages/utils/data.json'
-// const {authors , ...others} = data
-{/* <div className='flex justify-evenly mt-[2rem]'>
- {authors.map((data)=> (
-   ))}
-  </div> */}
-  {/* data={data} */}
-import {CardCmp} from "@/pages/components/CardComponent/CardCmp";
 
 
-export default function Home() {
-  console.log(authors);
+  import CardCmp from "./components/CardComponent/CardCmp";
+
+ function Home () {
+  // console.log(authors);
   return (
     <>
-      <CardCmp  />
+    hasan ashraf is me to bhand agaya hai naya bana na parhay ga
+    
+      {/* <CardCmp  /> */}
    
     </>
   );
 }
+
+export default Home
