a6dfcf8210b599fbe4b80a9348a9b47cf12dfbf9
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-16 01:28:07 +0500

    maha walay error hai next mai

--- datadi/pages/components/CardComponent/CardCmp.jsx
+++ datadi/pages/components/CardComponent/CardCmp.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from 'react';
-import { Card } from 'antd';
+import React, { useState } from "react";
+import { Card } from "antd";
 const { Meta } = Card;
 import { AiOutlineLike } from "react-icons/ai";
 import { LiaCommentSolid } from "react-icons/lia";
-const CardCmp = ({data}) => {
+const CardCmp = ({ data }) => {
   console.log(data);
- 
-  return(
+
+  return (
     <Card
-    hoverable
-    style={{
-      width: 240,
-      marginBottom: 20,
-    }}
-    cover={<img alt="example" src="https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710499747/branding%20hopes/WhatsApp_Image_2024-03-15_at_14.34.05_25bf20d3_iwzafm.jpg" />}
-  >
-    <Meta title={`${data?.firstName} ${data?.lastName}` } description={data?.phone} />
-   
-   <div className='flex mt-[1rem] items-center justify-between'>
-   <div className='flex items-center'>
-   <AiOutlineLike /> 
-   {data?.numLikes}
-   </div>
-   <div className='flex items-center'>
-   <LiaCommentSolid />
-   {data?.numComments}
-   </div>
-   </div>
-  </Card>
-  )
+      hoverable
+      style={{
+        width: 240,
+        marginBottom: 20,
+      }}
+      cover={
+        <img
+          alt="example"
+          src="https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710499747/branding%20hopes/WhatsApp_Image_2024-03-15_at_14.34.05_25bf20d3_iwzafm.jpg"
+        />
+      }
+    >
+      <Meta
+        title={`${data?.firstName} ${data?.lastName}`}
+        description={data?.phone}
+      />
+
+      <div className="flex mt-[1rem] items-center justify-between">
+        <div className="flex items-center">
+          <AiOutlineLike />
+          {data?.numLikes}
+        </div>
+        <div className="flex items-center">
+          <LiaCommentSolid />
+          {data?.numComments}
+        </div>
+      </div>
+    </Card>
+  );
 };
 export default CardCmp;
