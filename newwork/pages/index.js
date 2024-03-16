c4bb0897c5c24c0eb55c1d88cc8ba6cf6e7195a7
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-16 23:09:08 +0500

    static routes

--- newwork/pages/index.js
+++ newwork/pages/index.js
@@ -1,29 +1,9 @@
+import React from 'react'
 
-import Button from "./components/button/Button";
-import Lists from "./components/list/Lists";
-import { useState } from "react";
-
-export default function Home() {
-  const [dataOfArray , setDataOfArray] = useState(['be honest' , 'tell truth' ,'never cheat'])
-  // let dataOfArray = 
-  const addHandler = () => {
-    console.log('click working')
-    setDataOfArray([...dataOfArray, 'new item added']);
-    // setDataOfArray.push([...dataOfArray , 'new items add'])
-    console.log('dataOfArray' , dataOfArray)
-  }
+const index = () => {
   return (
-    <>
-      <h1>
-        Welcome to 2nd video practice with Ishaq Bhojani
-      </h1>
-     {dataOfArray.map((items)=> (
-      <Lists data={items} key={items}/>
-     ))}
-      <h3>
-        Oper map ke zariye data display keraya jaraha hai 
-      </h3>
-      <Button text='click this button to new list' click={addHandler} />
-    </>
-  );
+    <div>New work Static Routes and Dynamic Routes</div>
+  )
 }
+
+export default index
